8e07cfce3d4939f3b673723ddc7ce9877d23ec2a
Quintans <vitor.isilva@sptech.school>
2023-10-03 22:47:53 -0300

    Validações Feitas, e inserção da API Web-data-viz (Login)

--- Site-Institucional/public/js/valicacoesCadastro.js
+++ Site-Institucional/public/js/valicacoesCadastro.js
@@ -1,27 +1,47 @@
 
+const campoCNPJ = document.querySelector('#input_cnpj');
+
+campoCNPJ.addEventListener('keypress', ()=> {
+    var inputlength = campoCNPJ.value.length
+
+    console.log(inputlength);
+
+    if(inputlength === 2 || inputlength === 6){
+        campoCNPJ.value += '.'
+    }else if (inputlength === 10){
+        campoCNPJ.value += '/'
+    }else if (inputlength === 15) {
+        campoCNPJ.value += '-'
+    }
+});
+
+
 function voltar() {
     window.location.href = "farmacos.html";
-
 }
 function cadastro() {
     var nome_Completo = input_Nome_Completo.value;
+    var cnpj = input_cnpj.value;
     var emailVar = input_email.value;
     var senhaVar = input_senha.value;
     var confirmarSenhaVar = input_confirmar_senha.value;
 
-    if (nome_Completo == "" ||emailVar == "" || senhaVar == "" || confirmarSenhaVar == "") {
+    if (nome_Completo == "" ||emailVar == "" || senhaVar == "" || confirmarSenhaVar == "" || cnpj == "") {
         alert("Preencha todos os campos!");
         document.getElementById('input_Nome_Completo').style.boxShadow ='0px 2px 0px 0px red'
         document.getElementById('input_email').style.boxShadow ='0px 2px 0px 0px red'
         document.getElementById('input_senha').style.boxShadow ='0px 2px 0px 0px red'
         document.getElementById('input_confirmar_senha').style.boxShadow ='0px 2px 0px 0px red'
+        document.getElementById('input_cnpj').style.boxShadow ='0px 2px 0px 0px red'
         return false;
     } else if (nome_Completo.length <= 2) {
-        alert('Seu nome tem que ter no mínimo 3 letras!!!')
+        alert('O nome da empresa deve ter no mínimo 3 letras!!!')
         document.getElementById('input_Nome_Completo').style.boxShadow ='0px 2px 0px 0px red'
         document.getElementById('input_email').style.boxShadow ='0px 2px 0px 0px 00000056'
         document.getElementById('input_senha').style.boxShadow ='0px 2px 0px 0px #00000056'
+        document.getElementById('input_cnpj').style.boxShadow ='0px 2px 0px 0px red'
         document.getElementById('input_confirmar_senha').style.boxShadow ='0px 2px 0px 0px #00000056'
+        document.getElementById('input_cnpj').style.boxShadow ='0px 2px 0px 0px #00000056'
         return false;
     } else if (emailVar.indexOf("@") == -1 || emailVar.indexOf(".com") == -1) {
         alert(`Email cadastrado é invalido!! Para cadastrar é preciso que o email tenha '@' e '.com'`)
@@ -29,6 +49,7 @@ function cadastro() {
         document.getElementById('input_email').style.boxShadow ='0px 2px 0px 0px red'
         document.getElementById('input_senha').style.boxShadow ='0px 2px 0px 0px #00000056'
         document.getElementById('input_confirmar_senha').style.boxShadow ='0px 2px 0px 0px #00000056'
+        document.getElementById('input_cnpj').style.boxShadow ='0px 2px 0px 0px #00000056'
         return false;
     } else if (senhaVar != confirmarSenhaVar) {
         alert(`Os campos de senha e confirmar senha estão diferentes`)
@@ -36,6 +57,7 @@ function cadastro() {
         document.getElementById('input_email').style.boxShadow ='0px 2px 0px 0px #00000056'
         document.getElementById('input_senha').style.boxShadow ='0px 2px 0px 0px red'
         document.getElementById('input_confirmar_senha').style.boxShadow ='0px 2px 0px 0px red'
+        document.getElementById('input_cnpj').style.boxShadow ='0px 2px 0px 0px #00000056'
         return false;
     } else {
         window.location.href = "login.html";
