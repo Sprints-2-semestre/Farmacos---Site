7dd38dc7e553e6dcd4886249dc752e35666a1544
Quintans <vitor.isilva@sptech.school>
2023-10-19 17:48:59 -0300

    Cadastro e Login Funcionando, Back-end Feito!

--- Site-Institucional/src/database/config.js
+++ Site-Institucional/src/database/config.js
@@ -7,7 +7,7 @@ var mySqlConfig = {
     host: "localhost",
     database: "farmacos",
     user: "root",
-    password: ""
+    password: "quintans123"
 };
 
 // // CONEXÃO DO SQL SERVER - AZURE (NUVEM)
