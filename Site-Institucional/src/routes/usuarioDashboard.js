99e604d6f47d13021ff40d9fcfdc3ad66c94926d
MarianaNdO <mariana.oliveira@sptech.school>
2023-11-13 13:31:02 -0300

    Trabalhando na lista de usuarios

--- Site-Institucional/src/routes/usuarioDashboard.js
+++ Site-Institucional/src/routes/usuarioDashboard.js
@@ -13,6 +13,10 @@ router.post("/alterar", function (req, res) {
     usuarioDashboardController.alterar(req, res);
 });
 
+router.get("/listar/:fkAme", function(req, res) {
+    usuarioDashboardController.listar(req, res);
+ });
+ 
 
 router.get("/listar", function (req, res) {
     // função a ser chamada quando acessar /carros/listar
